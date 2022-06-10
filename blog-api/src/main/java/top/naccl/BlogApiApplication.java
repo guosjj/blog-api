be62bae342a0c2a984a2377118a1098e984e0b47
“guosj” <2547361543@qq.com>
2022-06-10 21:23:20 +0800

    修改配置

--- blog-api/src/main/java/top/naccl/BlogApiApplication.java
+++ blog-api/src/main/java/top/naccl/BlogApiApplication.java
@@ -1,9 +1,11 @@
 package top.naccl;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("top.naccl.mapper")
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
