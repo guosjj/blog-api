cb238c5069197c0278273bd26cc0d32c19a25b12
“guosj” <2547361543@qq.com>
2022-06-10 21:28:02 +0800

    first commit

---
+++ src/main/java/top/naccl/BlogApiApplication.java
@@ -0,0 +1,15 @@
+package top.naccl;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("top.naccl.mapper")
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+	}
+
+}
